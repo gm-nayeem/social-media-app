f6e6262100e3841c3007bff6c8ab184f334cfc25
golam mostafa <gmnayeem25@gmail.com>
2023-10-11 22:12:35 +0600

    add vercel

--- client/src/utils/makeRequest.js
+++ client/src/utils/makeRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const REACT_APP_API_URL = "https://zenshop.vercel.app/api";
-const REACT_APP_API_URL = "http://localhost:5000/api";
+const REACT_APP_API_URL = "https://social-media-app-backend-api.vercel.app/api";
+// const REACT_APP_API_URL = "http://localhost:5000/api";
 
         
 export const publicRequest = axios.create({
